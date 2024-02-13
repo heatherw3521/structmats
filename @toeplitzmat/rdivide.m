cf537ecc72d3aeb882fa897406b1dd81fa790a9a
lab192 <lab192@case.edu>
2024-02-13 07:08:30 -0800

    rdivide hotfix - the double ./ toeplitzmat case was handled incorrectly

--- @toeplitzmat/rdivide.m
+++ @toeplitzmat/rdivide.m
@@ -3,18 +3,19 @@ function h = rdivide(T, g)
 %   T ./ g pointwise divides T by g. 
 %   T and g can be scalars or TOEPLITZMATs
 
-if ( ~isa(T, 'toeplitzmat') ) % ??? ./ TOEPLITZMAT
-    h = rdivide(g, T);
+if ( isa(T, 'double') ) % DOUBLE ./ TOEPLITZMAT
+    h = toeplitzmat(T ./ g.tc, T ./ g.tr);
 
 elseif ( isa(g,'double') ) % TOEPLITZMAT ./ DOUBLE
     h = toeplitzmat(T.tc ./ g, T.tr ./ g);
 
-elseif ( ~isa(g,'toeplitzmat') ) % TOEPLITZMAT ./ ???
+% TOEPLITZMAT ./ ??? or ??? ./ TOEPLITZMAT
+elseif ( ~isa(g,'toeplitzmat') || ~isa(T,'toeplitzmat') )
     error( 'TOEPLITZMAT:rdivide:unknown', ...
         ['Undefined function ''rdivide'' for input arguments of type %s ' ...
         'and %s.'], class(T), class(g));
  
-else                           % TOEPLITZMAT + TOEPLITZMAT
+else                       % TOEPLITZMAT ./ TOEPLITZMAT
     %Size check
     if( ~isequal(size(T), size(g)) )
         error( 'TOEPLITZMAT:rdivide:sizemismatch', ...
