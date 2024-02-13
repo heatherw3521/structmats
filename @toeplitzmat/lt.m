d4c7c7b6cfc8c4e7988b23ce0306c223a142f629
lab192 <lab192@case.edu>
2024-02-13 09:48:27 -0800

    -

--- @toeplitzmat/lt.m
+++ @toeplitzmat/lt.m
@@ -3,6 +3,6 @@ function h = lt(T,g)
 %   T and g can be TOEPLITZMATs or scalars
 %   behaves the same as < for matrices in MATLAB
 
-ftest = @(x) isnumeric(x);
+ftest = @(x) (isnumeric(x) || (islogical(x)&&~isa(x,'toeplitzmat')));
 h = toepcompare(T, g, @(x,y) x < y, "<", ftest);
 end
