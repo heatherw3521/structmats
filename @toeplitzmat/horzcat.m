0ec7b9a46127102725a098b357d577588088784f
Levent <lab192@case.edu>
2024-02-22 15:49:43 -0800

    Updated concatenation operators to allow for multiple inputs

--- @toeplitzmat/horzcat.m
+++ @toeplitzmat/horzcat.m
@@ -1,43 +1,51 @@
-function C = horzcat(A, B)
+function C = horzcat(varargin)
 %HORZCAT Horizontal Concatenation for TOEPLITZMAT objects
  
-% Concatenating two TOEPLITZMAT objects
-if( isa(A, 'toeplitzmat') && isa(B, 'toeplitzmat'))
-    
-    % Check if sizes are compatible
-    if( size(A,1) ~= size(B,1) )
-        error( 'TOEPLITZMAT:horzcat:sizemismatch', ...
-            'Cannot horizontally concatenate Toeplitz matrices of sizes %s -by- %s and %s -by- %s.',...
-            ""+size(A,1),""+size(A,2), ""+size(B,1),""+size(B,2) );   
-    end
-    
-    % SPECIAL CASE: Result is also toeplitz
-    m = size(B,1);
-    n = size(A,2);
-    if(n >= m-1 && isequal( flip(A.tr(end-m+2:end)), B.tc(2:end).'))
-        C = toeplitzmat(A.tc, [A.tr B.tr]);
-    else
-        aflat = [flip(A.tc(2:end)') A.tr];
-        if(isequal( aflat(end-m+2:end), B.tc(2:end).'))
+if(size(varargin,2) == 1)
+    C = varargin{1};
+elseif(size(varargin,2) > 2)
+    C = [varargin{1} horzcat(varargin{2:end})]; 
+else
+    A = varargin{1};
+    B = varargin{2};
+
+    % Concatenating two TOEPLITZMAT objects
+    if( isa(A, 'toeplitzmat') && isa(B, 'toeplitzmat'))
+
+        % Check if sizes are compatible
+        if( size(A,1) ~= size(B,1) )
+            error( 'TOEPLITZMAT:horzcat:sizemismatch', ...
+                'Cannot horizontally concatenate Toeplitz matrices of sizes %s -by- %s and %s -by- %s.',...
+                ""+size(A,1),""+size(A,2), ""+size(B,1),""+size(B,2) );   
+        end
+
+        % SPECIAL CASE: Result is also toeplitz
+        m = size(B,1);
+        n = size(A,2);
+        if(n >= m-1 && isequal( flip(A.tr(end-m+2:end)), B.tc(2:end).'))
             C = toeplitzmat(A.tc, [A.tr B.tr]);
         else
-            C = [toeplitz(A.tc, A.tr) toeplitz(B.tc, B.tr)];
+            aflat = [flip(A.tc(2:end)') A.tr];
+            if(isequal( aflat(end-m+2:end), B.tc(2:end).'))
+                C = toeplitzmat(A.tc, [A.tr B.tr]);
+            else
+                C = [toeplitz(A.tc, A.tr) toeplitz(B.tc, B.tr)];
+            end
+        end
+
+    % Concatenating TOEPLITZMAT with something else
+    else
+
+        if( isempty(A)) % [TOEPLITZMAT [] ]
+            C = B;
+        elseif ( isempty(B) ) % [ [] TOEPLITZMAT]
+            C = A;
+        elseif ( isa(A, 'toeplitzmat') ) % Attempt the concatenation 
+            C = [toeplitz(A.tc, A.tr) B];
+        else
+            C = [A toeplitz(B.tc, B.tr)];
         end
     end
-    
-% Concatenating TOEPLITZMAT with something else
-else
-    
-    if( isempty(A)) % [TOEPLITZMAT [] ]
-        C = B;
-    elseif ( isempty(B) ) % [ [] TOEPLITZMAT]
-        C = A;
-    elseif ( isa(A, 'toeplitzmat') ) % Attempt the concatenation 
-        C = [toeplitz(A.tc, A.tr) B];
-    else
-        C = [A toeplitz(B.tc, B.tr)];
-    end
 end
-
 end
 
--- @toeplitzmat/vertcat.m
+++ @toeplitzmat/vertcat.m
@@ -1,41 +1,50 @@
-function C = vertcat(A, B)
+function C = vertcat(varargin)
 %HORZCAT Vertical Concatenation for TOEPLITZMAT objects
  
-% Concatenating two TOEPLITZMAT objects
-if( isa(A, 'toeplitzmat') && isa(B, 'toeplitzmat'))
-    
-    % Check if sizes are compatible
-    if( size(A,2) ~= size(B,2) )
-        error( 'TOEPLITZMAT:vertcat:sizemismatch', ...
-            'Cannot vertically concatenate Toeplitz matrices of sizes %s -by- %s and %s -by- %s.',...
-            ""+size(A,1),""+size(A,2), ""+size(B,1),""+size(B,2) );   
-    end
-    
-    % SPECIAL CASE: Result is also toeplitz
-    n = size(B,2);
-    m = size(A,1);
-    if(m >= n-1 && isequal( flip(A.tc(end-n+2:end)), B.tr(2:end).'))
-        C = toeplitzmat([A.tc;B.tc], A.tr);
-    else
-        aflat = [flip(A.tc(2:end)') A.tr];
-        if(isequal( aflat(1:n-1), B.tr(2:end)))
+if(size(varargin,2) == 1)
+    C = varargin{1};
+elseif(size(varargin,2) > 2)
+    C = [varargin{1} ; vertcat(varargin{2:end})]; 
+else
+    A = varargin{1};
+    B = varargin{2};
+
+    % Concatenating two TOEPLITZMAT objects
+    if( isa(A, 'toeplitzmat') && isa(B, 'toeplitzmat'))
+
+        % Check if sizes are compatible
+        if( size(A,2) ~= size(B,2) )
+            error( 'TOEPLITZMAT:vertcat:sizemismatch', ...
+                'Cannot vertically concatenate Toeplitz matrices of sizes %s -by- %s and %s -by- %s.',...
+                ""+size(A,1),""+size(A,2), ""+size(B,1),""+size(B,2) );   
+        end
+
+        % SPECIAL CASE: Result is also toeplitz
+        n = size(B,2);
+        m = size(A,1);
+        if(m >= n-1 && isequal( flip(A.tc(end-n+2:end)), B.tr(2:end).'))
             C = toeplitzmat([A.tc;B.tc], A.tr);
         else
-            C = [toeplitz(A.tc, A.tr);toeplitz(B.tc, B.tr)];
+            aflat = [flip(A.tc(2:end)') A.tr];
+            if(isequal( aflat(1:n-1), B.tr(2:end)))
+                C = toeplitzmat([A.tc;B.tc], A.tr);
+            else
+                C = [toeplitz(A.tc, A.tr);toeplitz(B.tc, B.tr)];
+            end
         end
-    end
-    
-% Concatenating TOEPLITZMAT with something else
-else
-    
-    if( isempty(A)) % [TOEPLITZMAT [] ]
-        C = B;
-    elseif ( isempty(B) ) % [ [] TOEPLITZMAT]
-        C = A;
-    elseif ( isa(A, 'toeplitzmat') ) % Attempt the concatenation 
-        C = [toeplitz(A.tc, A.tr);B];
+
+    % Concatenating TOEPLITZMAT with something else
     else
-        C = [A;toeplitz(B.tc, B.tr)];
+
+        if( isempty(A)) % [TOEPLITZMAT [] ]
+            C = B;
+        elseif ( isempty(B) ) % [ [] TOEPLITZMAT]
+            C = A;
+        elseif ( isa(A, 'toeplitzmat') ) % Attempt the concatenation 
+            C = [toeplitz(A.tc, A.tr);B];
+        else
+            C = [A;toeplitz(B.tc, B.tr)];
+        end
     end
 end
 
