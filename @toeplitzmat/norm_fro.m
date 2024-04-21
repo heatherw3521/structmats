27ae771190642a33e037842cc14c783d93200254
lab192 <lab192@case.edu>
2024-04-21 16:43:32 -0700

    Toeplitz frobenius norm fix. Rectangular case was handled improperly.

--- @toeplitzmat/norm_fro.m
+++ @toeplitzmat/norm_fro.m
@@ -5,8 +5,8 @@ function h = norm_fro(T)
 
     m = size(T,1);
     n = size(T,2);
-    subsum = sum((m:-1:1)' .* (T.tc).^2); % Contribution on & below the diag
-    supsum = sum((n-1:-1:1) .* (T.tr(2:end)).^2); % Contribution on & above diag
+    subsum = sum((min(n,m:-1:1))' .* (T.tc).^2); % Contribution on & below the diag
+    supsum = sum((min(m,n-1:-1:1)) .* (T.tr(2:end)).^2); % Contribution on & above diag
     h = sqrt(subsum + supsum);
 end
 
