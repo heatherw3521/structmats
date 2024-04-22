e802d3339f2e313c911104c3d702ad6ef09e24a3
lab192 <lab192@case.edu>
2024-04-21 17:01:16 -0700

    Added absolute value operation for diagonally structured matrices

--- @diagonalstructure/not.m
+++ @diagonalstructure/not.m
@@ -3,7 +3,7 @@ function h = not(T)
 %   T is a DIAGONALSTRUCTURE
 %   behaves the same as ~ for matrices in MATLAB
 
-% little hack because I'm too lazy to change toepcompare
+% little hack
 % we can do logic on numerical data too
 ftest = @(x) true;
 h = diag_apply(@(x,y) ~x, ftest, "~", T, 1);
