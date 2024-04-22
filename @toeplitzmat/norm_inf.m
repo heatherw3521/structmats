e01418f0cb4f74b890208252b6ec498d65e2b5ef
lab192 <lab192@case.edu>
2024-04-21 17:34:34 -0700

    Infinity norm fixed for toeplitz matrices. The rectangular case was a bit more tedious than expected...

--- @toeplitzmat/norm_inf.m
+++ @toeplitzmat/norm_inf.m
@@ -2,8 +2,15 @@ function h = norm_inf(T)
 %NORM_INF Efficiently computes the infinity-norm for Toeplitz matrices. 
 %   The inf-norm is the maximum absolute row sum.
 
-    rs = [flip(cumsum(abs(T.tr(2:end)))) 0]; % super-diag partial sums
-    cs = [0 cumsum(abs(T.tc(2:end)))']; % sub-diag partial sums
-    h = T.tc(1) + max(rs+cs);
+    % Get dimensions of T
+    m = size(T,1);
+    n = size(T,2);
+
+    rs = [T.tr(n:-1:1).';T.tc(2:m-n+1)];
+    rs = rs(1:m-1); % Rightmost column of T, excluding last element
+    ls = [T.tc(2:end)]; % Leftmost column of T, excluding first element
+    d = [0 ; cumsum(abs(ls)-abs(rs))]; % Difference of consecutive row sums
+    rowsums = sum(abs(T.tr)) + d;
+    h = max(rowsums);
 end
 
