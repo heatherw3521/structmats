0b539c2c6c7ea9aadc0341afe7777e76e053c635
lab192 <lab192@case.edu>
2024-04-21 17:39:11 -0700

    1 Norm fixed

--- @toeplitzmat/norm_1.m
+++ @toeplitzmat/norm_1.m
@@ -1,12 +1,10 @@
 function h = norm_1(T)
 %NORM_1 Efficiently computes the 1-norm for Toeplitz matrices.
 %   The 1-norm is the maximum absolute column sum. 
-% 
-%   For toeplitz matrices, the 1-norm and infinity-norm are the same! This
-%   can most easily be seen by the fact that ||A||_1 = ||A.'||_inf, and the
-%   symmetry in the inf-norm algorithm, which is invariant under swapping
-%   T.tc and T.tr.
+%
+%   We compute this by noting that the the maximum absolute column sum is
+%   the maximum absolute row sum (infinity-norm!) of the transpose.
 
-    h = norm_inf(T);
+    h = norm(T.', 'inf');
 end
 
