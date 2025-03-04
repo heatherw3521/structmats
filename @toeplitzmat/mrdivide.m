03a791820daa5aed59685ce4cd3d6fe68485b285
heatherw3521 <heatherw3521@gmail.com>
2025-03-04 15:30:32 -0800

    update mrdivide

--- @toeplitzmat/mrdivide.m
+++ @toeplitzmat/mrdivide.m
@@ -3,7 +3,7 @@ function x = mrdivide(T, b)
 %   Solves the equations xT = b.
 %   (note: equivalent to T'x' = (xT)' = b')
 
-error('Toeplitz right-divide implementation in progress!')
+%error('Toeplitz right-divide implementation in progress!')
 x = (T' \ b')';
 end
 
