cb09bf680b8cd26f8cbeb5c2f314f2ffdca94f51
lab192 <lab192@case.edu>
2025-03-12 17:18:48 -0700

    Small documentation update

--- @vandermat/numericalrank_bound.m
+++ @vandermat/numericalrank_bound.m
@@ -9,6 +9,8 @@ end
 % Analytical bound derived in https://arxiv.org/abs/1609.09494 by 
 % Beckerman and Townsend (2019)
 r = 2*(1+ceil((4*log(8*floor(V.n/2)/pi)*log(4/eps)) / (pi^2)) );
+% This seems like a worst-case bound that can be much improved by solving
+% the discrete problem
 
 end
 
